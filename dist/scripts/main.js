36944cd4936d4563f3ec5d970e4eedbbb41d778f
Chelsea Franz <chelseafranz3@gmail.com>
2014-12-20 13:24:53 -0500

    gh pages test

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('The Iron Yard Rocks');
+
+// down arrow animation
+// $('.down').mouseenter(function  () {
+// 	$('.down').addClass('animated bounce');
+// });
+// $('.down').mouseleave(function  () {
+// 	$('.down').removeClass('animated bounce');
+// });
+
