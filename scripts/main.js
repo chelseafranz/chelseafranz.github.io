ac8a6624b7fee57ae8f0721b6f739aee9c61e341
Chelsea Franz <chelseafranz3@gmail.com>
2016-06-11 14:03:30 -0600

    update layout

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-
-// down arrow animation
-// $('.down').mouseenter(function  () {
-// 	$('.down').addClass('animated bounce');
-// });
-// $('.down').mouseleave(function  () {
-// 	$('.down').removeClass('animated bounce');
-// });
-
